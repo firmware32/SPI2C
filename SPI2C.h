d44bbf4e5b08fde92c09479a2099ee137428586e
Doug Reuter <firmware32com@gmail.com>
2016-01-22 14:00:29 -0500

    added license info

--- SPI2C.h
+++ SPI2C.h
@@ -1,3 +1,22 @@
+//	written by : Doug Reuter
+//	SPI2C is a libarary that combines both SPI calls and I2C calls into 
+// like methods so sensors that use both protocols can be used with minimum modfication
+//	may work on other arduino boards as well
+//	http://Firmware32.com   https://github.com/firmware32/SPI2C/
+//	Copyright (C) 2016 Doug Reuter   dougreuter@gmail.com
+
+//	This program is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+
+//	You should have received a copy of the GNU General Public License
+//	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // SPI2C.h
 
 #ifndef _SPI2C_h
